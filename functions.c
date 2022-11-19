88c278824ae0521138be31ed3c31d2a8e513db2b
Marco Antonio Rodrigues Gomes de Souza <marcoantoniorodrigues200201@gmail.com>
2022-11-19 20:52:32 -0300

    mudancas

--- functions.c
+++ functions.c
@@ -2,31 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void cadastrar_aluno(aluno *raiz,int matricula,char *nome,char* telefone,char *email){
-  if(raiz == NULL){
-    aluno *new_aluno = malloc(sizeof(aluno));
-    raiz = new_aluno;
-  }else{
-    buscar_dados_aluno(raiz,matricula); 
-  }
-}
+// void cadastrar_aluno(int matricula){
+//   if(raiz == NULL){
+//     aluno *new_aluno = malloc(sizeof(aluno));
+//     raiz = new_aluno;
+//   }else{
+//     buscar_dados_aluno(raiz,matricula); 
+//   }
+// }
 
-void remover_aluno(aluno *raiz,int matricula);
+// void remover_aluno(aluno *raiz,int matricula);
 
-void editar_aluno(aluno *raiz,int matricula);
+// void editar_aluno(aluno *raiz,int matricula);
 
-aluno* buscar_dados_aluno(aluno *raiz,int matricula);
+// aluno* buscar_dados_aluno(aluno *raiz,int matricula);
 
-aluno *read_banco_de_dados(FILE *banco_de_dados){
-  aluno *raiz = malloc(sizeof(aluno));
-  int aux;
-  fscanf(banco_de_dados,"%i",&aux);
-  aluno->matricula = aux;
-}
+int main(){
 
-void write_banco_de_dados(aluno *raiz,FILE *banco_de_dados);
-
-void main(){
-  FILE *banco_de_dados = fopen("banco_de_dados.txt","r");
-
+  return 0;
 }
--- lib.h
+++ lib.h
@@ -12,18 +12,18 @@ typedef struct disciplina{
 
 typedef struct aluno{
   int matricula;
-  char *nome;
-  char *telefone;
-  char *email;
-  int semestre;
-  disciplina *disciplinas;
+  // char *nome;
+  // char *telefone;
+  // char *email;
+  // int semestre;
+  // disciplina *disciplinas;
   aluno  *filho_esq;
   aluno  *filho_dir;
 }aluno;
 
 aluno *raiz = NULL;
 
-void cadastrar_aluno(aluno *raiz,int matricula,char *nome,char* telefone,char *email);
+void cadastrar_aluno(int matricula);//falta char *nome,char* telefone,char *email
 
 void remover_aluno(aluno *raiz,int matricula);
 
@@ -31,8 +31,8 @@ void editar_aluno(aluno *raiz,int matricula);
 
 aluno* buscar_dados_aluno(aluno *raiz,int matricula);
 
-aluno *read_banco_de_dados(FILE *banco_de_dados);
+// aluno *read_banco_de_dados(FILE *banco_de_dados);
 
-void write_banco_de_dados(aluno *raiz,FILE *banco_de_dados);
+// void write_banco_de_dados(aluno *raiz,FILE *banco_de_dados);
 
 #endif
