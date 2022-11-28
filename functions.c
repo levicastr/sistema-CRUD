90ba4bdad1d2b5dd5c7f338807450118f21e8e8f
Marco Antonio Rodrigues Gomes de Souza <marcoantoniorodrigues200201@gmail.com>
2022-11-28 18:36:25 -0300

    update

--- functions.c
+++ functions.c
@@ -1,22 +1,9 @@
 #include "lib.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-// void cadastrar_aluno(aluno *raiz,int matricula,char* nome,char* numero, char* email,disciplina** disciplinas){
-
-// }
-
-
-// aluno remover_aluno(aluno *raiz,int matricula){
-
-// }
-
-
-// void editar_aluno(aluno *raiz,int matricula){
-
-// }
-
-aluno* buscar_dados_aluno(aluno* raiz,int matricula){
+aluno* buscar_dados_aluno(aluno* raiz, int matricula){
     if(raiz == NULL){
         return NULL; //vazia
     }else if(matricula == raiz->matricula){
@@ -36,23 +23,106 @@ aluno* buscar_dados_aluno(aluno* raiz,int matricula){
     }
 }
 
-// aluno *read_banco_de_dados(FILE *banco_de_dados){
+void cadastrar_aluno(int matricula,char* nome,char* numero, char* email,int quantidade_disciplinas, disciplina **disciplinas,endereco* endereco_aluno){//ok
+    aluno* aux = buscar_dados_aluno(raiz, matricula);
+    if(aux == NULL || aux->matricula != matricula){// vazia ou eu posso adicionar
+        aluno* novo_aluno = malloc (sizeof(aluno));
+        novo_aluno->matricula = matricula;
+        novo_aluno->nome_aluno = malloc(sizeof(char)*200);
+        novo_aluno->numero_aluno = malloc(sizeof(char)*200);
+        novo_aluno->email_aluno = malloc(sizeof(char)*200);
 
-// }
+        strcpy(novo_aluno->nome_aluno,nome);
+        strcpy(novo_aluno->numero_aluno,numero);
+        strcpy(novo_aluno->email_aluno,email);
+        novo_aluno->endereco_aluno = endereco_aluno;
+        novo_aluno->quantidade_disciplinas = quantidade_disciplinas;
+        novo_aluno->disciplinas = disciplinas;
+        novo_aluno->filho_esq = NULL;
+        novo_aluno->filho_dir = NULL;
+        quantidade_alunos++;
+        if(aux == NULL){ //add na raiz
+            raiz = novo_aluno;
+        }else{
+            if(matricula < aux->matricula){
+                aux->filho_esq = novo_aluno;
+            }else{
+                aux->filho_dir = novo_aluno;
+            }
+        }
+    }else{//nao posso deixar add novamente pq neste caso havera chaves duplicadas
+        printf("Cadastro inválido. Chave duplicada\n");
+    }
+}
 
-// void write_banco_de_dados(aluno *raiz,FILE *banco_de_dados){
+aluno remover_aluno(int matricula);
 
-// }
+void editar_aluno(int matricula);
+
+void imprimir_aluno(int matricula);
+
+void read_banco_de_dados(FILE *banco_de_dados){//ok
+    int aux, matricula, codigo_disciplina;
+    float nota_final_disciplina;
+    char nome[200], numero[200], email[200],nome_disciplina[200] ;  
+    fscanf(banco_de_dados,"%i",&aux);
+    for(int i = aux; 0 < i;i--){
+        int quantidade_disciplinas = 0;
+        fscanf(banco_de_dados,"%i",&matricula);
+        fscanf(banco_de_dados,"%s",nome);
+        fscanf(banco_de_dados,"%s",numero);
+        fscanf(banco_de_dados,"%s",email);
+        endereco* endereco = malloc(sizeof(endereco));
+        endereco->nome_rua = malloc(sizeof(char)*200);
+        fscanf(banco_de_dados,"%i",&endereco->numero_casa);
+        fscanf(banco_de_dados,"%s",endereco->nome_rua);
+        fscanf(banco_de_dados,"%i",&endereco->cep);
+
+        fscanf(banco_de_dados,"%i",&quantidade_disciplinas);
+        disciplina** disciplinas = malloc(sizeof(disciplina*)*quantidade_disciplinas);//ponteiro de disciplina
+        for(int y = 0; y < quantidade_disciplinas;y++){
+            disciplinas[y] = malloc(sizeof(disciplina));
+            disciplinas[y]->nome_disciplina = malloc(sizeof(char)*200);
+            fscanf(banco_de_dados,"%i",&disciplinas[y]->codigo_disciplina);
+            fscanf(banco_de_dados,"%s",disciplinas[y]->nome_disciplina);
+            fscanf(banco_de_dados,"%f",&disciplinas[y]->nota_final_disciplina);
+        }
+        cadastrar_aluno(matricula,nome,numero,email,quantidade_disciplinas,disciplinas,endereco);
+    }
+}
+
+void in_ordem(aluno* aux, FILE *backup){
+    if(aux == NULL){
+        return;
+    }else{
+        in_ordem(aux->filho_esq,backup);
+        fprintf(backup,"\n%i",aux->matricula);
+        fprintf(backup,"\n%s",aux->nome_aluno);
+        fprintf(backup,"\n%s",aux->numero_aluno);
+        fprintf(backup,"\n%s",aux->email_aluno);
+        fprintf(backup,"\n%i",aux->endereco_aluno->numero_casa);
+        fprintf(backup,"\n%s",aux->endereco_aluno->nome_rua);
+        fprintf(backup,"\n%i",aux->endereco_aluno->cep);
+        fprintf(backup,"\n%i",aux->quantidade_disciplinas);
+        for(int i = 0; i< aux->quantidade_disciplinas ;i++){
+            fprintf(backup,"\n%i",aux->disciplinas[i]->codigo_disciplina);
+            fprintf(backup,"\n%s",aux->disciplinas[i]->nome_disciplina);
+            fprintf(backup,"\n%f",aux->disciplinas[i]->nota_final_disciplina);
+        }
+        in_ordem(aux->filho_dir,backup);
+    }
+}
+void write_banco_de_dados(FILE *backup){
+    fprintf(backup,"%i",quantidade_alunos);
+    in_ordem(raiz, backup);
+}
 
 int main(){
-  aluno *eu = malloc(sizeof(aluno));
-  eu->matricula = 511354;
-  eu->filho_dir = NULL;
-  eu->filho_esq = NULL;
-  aluno *tu = malloc(sizeof(aluno));
-  raiz = eu;
-  tu = buscar_dados_aluno(raiz,511354);
-  printf("\n%i\n",tu->matricula);
-
+    FILE* banco_de_dados = fopen("banco_de_dados.txt","r");
+    FILE* backup = fopen("backup.txt","w");
+    read_banco_de_dados(banco_de_dados);
+    write_banco_de_dados(backup);
+    fclose(banco_de_dados);
+    fclose(backup);
   return 0;
 }
--- lib.h
+++ lib.h
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifndef LIB
 #define LIB
 
 typedef struct disciplina{
-  int codigo;
-  char *nome;
-  float nota_final;
+  int codigo_disciplina;
+  char* nome_disciplina;
+  float nota_final_disciplina;
 }disciplina;
 
 typedef struct endereco{
-  char* rua;
-  int numero;
+  int numero_casa;
+  char* nome_rua;
   int cep;
 }endereco;
 
@@ -21,28 +22,31 @@ typedef struct aluno{
   char* nome_aluno;
   char* numero_aluno;
   char* email_aluno;
-  endereco *endereco_aluno;
+  int quantidade_disciplinas;
+  endereco* endereco_aluno;
   disciplina** disciplinas;
   struct aluno  *filho_esq;
   struct aluno  *filho_dir;
 }aluno;
 
 aluno *raiz = NULL;
+int quantidade_alunos = 0;
 
 // você deverá receber como parâmetro os dados de um aluno e adicionar estes dados
 // em uma Árvore Binária de Busca. Você deve considerar a matrícula do aluno como chave 
 // única do nó a ser inserido;
-void cadastrar_aluno(aluno *raiz,int matricula,char* nome,char* numero, char* email,disciplina** disciplinas);
+void cadastrar_aluno(int matricula,char* nome,char* numero, char* email,int quantidade_disciplinas, disciplina **disciplinas,endereco* endereco_aluno);
 
 // você deve receber como parâmetro a matrícula de um aluno e remover a nó da Árvore
 // Binária de Busca que contenha esta matrícula.
-aluno remover_aluno(aluno *raiz,int matricula);
+// retorna o aluno removido
+aluno remover_aluno(int matricula);
 
 // você deve receber como parâmetro a matrícula de um aluno e encontrar o nó da
 // Árvore Binária de Busca que contenha esta matrícula. Após encontrar o nó, você deve perguntar 
 // qual dos dados pessoais definidos pela equipe será alterado e solicitar o novo valor para 
 // realizar a alteração. Note que esta função permite que seja alterado apenas os dados pessoais do aluno.
-void editar_aluno(aluno *raiz,int matricula);
+void editar_aluno(int matricula);
 
 // você deve receber como parâmetro a matrícula de um aluno e encontrar o nó
 // da Árvore Binária de Busca que contenha esta matrícula. Após encontrar o nó, você deve 
@@ -50,12 +54,17 @@ void editar_aluno(aluno *raiz,int matricula);
 // que o aluno tenha cursado. As disciplinas e notas devem ser apresentadas em ordem decrescente 
 // de notas e portanto você deve aplicar um algoritmo de ordenação(obrigatoriamente) para 
 // ordenar o vetor, antes de imprimi-lo.
-aluno* buscar_dados_aluno(aluno *raiz,int matricula);
+void imprimir_aluno(int matricula);
+
+// busca o aluno na arvore e retorna a struct aluno
+aluno* buscar_dados_aluno(aluno* raiz,int matricula);
 
 // Ler o arquivo texto e ja preenche a arvore
-aluno *read_banco_de_dados(FILE *banco_de_dados);
+void read_banco_de_dados(FILE *banco_de_dados);
+
+void in_ordem(aluno* aux, FILE *backup);
 
 // salva a arvore no arquivo texto
-void write_banco_de_dados(aluno *raiz,FILE *banco_de_dados);
+void write_banco_de_dados(FILE *backup);
 
 #endif
